70c13aeb568056ee58e08f5b08e0a0dc90fc23d6
PretzelOptician <alekschultz1@gmail.com>
2022-06-06 16:26:56 -0400

    user info api call setup

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,25 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express")
+const router = express.Router()
+const db = require("./firebase")
+
+const {getDocs, getDoc, collection} = require("firebase/firestore")
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/info', async (req, res, next) => {
+  const allData = []; 
+  const docs = await getDocs(collection(db, "users"))
+  docs.forEach((doc) => {
+    let newVar = doc.data(); 
+    newVar.id = doc.id; 
+    allData.push(newVar); 
+  })
+  res.json({result:allData}); 
 });
+
+router.get("/info/:id", async (req,res,next) => {
+  getDoc(doc(db, "users", req.params.id))
+  .then((doc) => {res.send(doc.data())})
+})
 
 module.exports = router;
